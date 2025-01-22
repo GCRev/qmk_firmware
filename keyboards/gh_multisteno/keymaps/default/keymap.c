523c449110c81b59005ef8ff60343d0c55b6514a
Graham Held <graham.held@gmail.com>
2025-01-22 01:43:23 -0800

    Add left-hand sym layer toggle

--- keyboards/gh_multisteno/keymaps/default/keymap.c
+++ keyboards/gh_multisteno/keymaps/default/keymap.c
@@ -14,13 +14,18 @@ enum Layer {
 
 enum custom_keycodes {
     STN_ESC = SAFE_RANGE,
+    TO_STN,
     MO_FUN
 };
 
 // left-handed space combo
 const uint16_t PROGMEM lh_space_combo[] = { MO_FUN, KC_LSFT, COMBO_END };
+
+// left-handed sym access (for numpad with left hand)
+const uint16_t PROGMEM lh_sym_combo[] =   { TO_STN, KC_Z,    COMBO_END };
 combo_t key_combos[] = {
-    COMBO(lh_space_combo, KC_SPC)
+    COMBO(lh_space_combo, KC_SPC),
+    COMBO(lh_sym_combo,   TG(_SYM))
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -41,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,     KC_1,    KC_2,     KC_3,    KC_4,     KC_5,    KC_6,   KC_7,     KC_8,    KC_9,   KC_0,    KC_BSPC,
         KC_TAB,     KC_Q,    KC_W,     KC_E,    KC_R,     KC_T,    KC_Y,   KC_U,     KC_I,    KC_O,   KC_P,    KC_BSLS,
         KC_LCTL,    KC_A,    KC_S,     KC_D,    KC_F,     KC_G,    KC_H,   KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-        TO(_STENO), KC_Z,    KC_X,     KC_C,    KC_V,     KC_B,    KC_N,   KC_M,     KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
+        TO_STN,     KC_Z,    KC_X,     KC_C,    KC_V,     KC_B,    KC_N,   KC_M,     KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
                                        KC_LALT, MO_FUN,   KC_LSFT, KC_SPC, MO(_SYM), KC_LWIN
     ),
 
@@ -140,6 +145,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
     switch(keycode) {
     case STN_ESC:
         // run immediately only press
@@ -154,6 +160,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         } else {
             layer_off(_FUN);
         }
+        return false;
+    case TO_STN:
+        if (record->event.pressed) {
+            layer_move(_STENO);
+        } else {
+            // do nothing
+        }
+        return false;
     default:
         return true;
     }
